1a10a1690da0104c80dc4faf4689a1813892916b
Brian Zhao <bzspartan@gmail.com>
2017-04-20 15:11:42 -0700

    finished regression homework

--- src/regression.py
+++ src/regression.py
@@ -16,13 +16,13 @@
 
 # The modules we're going to use
 from __future__ import print_function
+
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn import datasets, linear_model
-from scipy import linalg
+from numpy.linalg import inv
 import numpy as np
 import matplotlib.pyplot as plt
-from pprint import pprint
 import warnings
 
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
@@ -30,6 +30,13 @@ warnings.filterwarnings(action="ignore", module="scipy", message="^internal gels
 
 def calcError(actual, predicted):
     return np.mean(((actual - predicted) ** 2)) / 2.0
+
+def plot(actual, predicted):
+    plt.scatter(actual.flatten(), predicted.flatten(), color='black')
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
+    plt.clf()
 
 
 if __name__ == '__main__':
@@ -50,7 +57,7 @@ if __name__ == '__main__':
     # <h4>Use scikit-learn library in the following cell</h4>
 
     # In[3]:
-
+    print("Using sci-kit learn")
     # Task 1-1: use linear regression in sklearn
     regr = linear_model.LinearRegression()
     regr.fit(train_data, train_target)
@@ -66,84 +73,96 @@ if __name__ == '__main__':
     print('Mean Squared Testing Error: ', calcError(test_target, test_predict))
 
     # Task 1-4: show plot a figure to show your predictions and real prices on the testing dataset
-    plt.scatter(test_target.flatten(), test_predict.flatten(), color='black')
-    plt.xticks(())
-    plt.yticks(())
-    plt.show()
+    plot(test_target, test_predict)
 
 
-    #
-    # # <h4>Use analytical solution (normal equation) to perform linear regression in the following cell</h4>
-    #
-    # # In[4]:
-    #
-    # # Task 2-1: Implement a function solving normal equation
-    # # Inputs: Training data and  training label
-    # # Output: Weights
-    # def myNormalEqualFun(X, y):
-    #     return
-    #
-    #
-    # # Task 2-2: Implement a function performing prediction
-    # # Inputs: Testing data and weights
-    # # Output: Predictions
-    # def myPredictFun(X, w):
-    #     return
-    #
-    #
-    # # Here we insert a column of 1s into training_data and test_data (to be consistent with our lecture slides)
-    # train_data_intercept = np.insert(train_data, 0, 1, axis=1)
-    # test_data_intercept = np.insert(test_data, 0, 1, axis=1)
-    #
-    # # Here we call myNormalEqual to train the model and get weights
-    # w = myNormalEqualFun(train_data_intercept, train_target)
-    #
-    # # Task 2-3: show intercept and coefficents
-    #
-    #
-    # # Task 2-4: show errors on training dataset and testing dataset
-    #
-    #
-    # # Task 2-5: show plot a figure to show your predictions and real prices on the testing dataset
-    #
-    #
-    # # <h4>Use numerical solution (gradient descent) to perform linear regression in the following cell</h4>
-    #
-    # # In[5]:
-    #
-    # # Feature scaling
-    # scaler = preprocessing.StandardScaler().fit(train_data)
-    # train_data = scaler.transform(train_data)
-    # test_data = scaler.transform(test_data)
-    #
-    #
-    # # Task 3-1: Implement a function performing gradient descent
-    # # Inputs: Training data, training label, leaerning rate, number of iterations
-    # # Output: Weights
-    # def myGradientDescentFun(X, y, learning_rate, numItrs):
-    #     return
-    #
-    #
-    # # Task 3-2: Implement a function performing prediction
-    # # Inputs: Testing data and weights
-    # # Output: Predictions
-    # def myPredictFun(X, w):
-    #     return
-    #
-    #
-    # # Here we insert a column of 1s into training_data and test_data (to be consistent with our lecture slides)
-    # train_data_intercept = np.insert(train_data, 0, 1, axis=1)
-    # test_data_intercept = np.insert(test_data, 0, 1, axis=1)
-    #
-    # # Here we call myGradientDescentFun to train the model and get weights
-    # # Note: you need to figure out good learning rate value and the number of iterations
-    # w = myGradientDescentFun(train_data_intercept, train_target, 0, 0)
-    #
-    # # Task 3-3: show intercept and coefficents
-    #
-    #
-    # # Task 3-4: show errors on training dataset and testing dataset
-    #
-    #
-    # # Task 3-5: show plot a figure to show your predictions and real prices on the testing dataset
-    #
+    # <h4>Use analytical solution (normal equation) to perform linear regression in the following cell</h4>
+    print("Using Analytical Solution: ")
+    # In[4]:
+
+    # Task 2-1: Implement a function solving normal equation
+    # Inputs: Training data and training label
+    # Output: Weights
+    def myNormalEqualFun(X, y):
+        x_transposed = np.transpose(X)
+        return inv(x_transposed.dot(X)).dot(x_transposed).dot(y)
+
+
+    # Task 2-2: Implement a function performing prediction
+    # Inputs: Testing data and weights
+    # Output: Predictions
+    def myPredictFun(X, w):
+        return X.dot(w)
+
+
+    # Here we insert a column of 1s into training_data and test_data (to be consistent with our lecture slides)
+    train_data_intercept = np.insert(train_data, 0, 1, axis=1)
+    test_data_intercept = np.insert(test_data, 0, 1, axis=1)
+
+    # Here we call myNormalEqual to train the model and get weights
+    w = myNormalEqualFun(train_data_intercept, train_target)
+
+    # Task 2-3: show intercept and coefficents
+    intercept = w[0]
+    weights = w[1:]
+    print('Intercept: ', intercept, '\nWeights: ', weights, '\n')
+
+    # Task 2-4: show errors on training dataset and testing dataset
+    train_predict = myPredictFun(train_data_intercept, w)
+    test_predict = myPredictFun(test_data_intercept, w)
+    print('Mean Squared Training Error: ', calcError(train_target, train_predict))
+    print('Mean Squared Testing Error: ', calcError(test_target, test_predict))
+
+    # Task 2-5: show plot a figure to show your predictions and real prices on the testing dataset
+    plot(test_target, test_predict)
+
+
+    # <h4>Use numerical solution (gradient descent) to perform linear regression in the following cell</h4>
+
+    # In[5]:
+
+    # Feature scaling
+    scaler = preprocessing.StandardScaler().fit(train_data)
+    train_data = scaler.transform(train_data)
+    test_data = scaler.transform(test_data)
+
+
+    # Task 3-1: Implement a function performing gradient descent
+    # Inputs: Training data, training label, leaerning rate, number of iterations
+    # Output: Weights
+    def myGradientDescentFun(X, y, learning_rate, numItrs):
+        w = np.random.random(size=(X.shape[1],1))
+        x_transpose_averaged = np.transpose(X) / X.shape[0]
+        for i in range(numItrs):
+            change = learning_rate * x_transpose_averaged.dot(y - X.dot(w))
+            w += change
+        return w
+
+    # Task 3-2: Implement a function performing prediction
+    # Inputs: Testing data and weights
+    # Output: Predictions
+    def myPredictFun(X, w):
+        return X.dot(w)
+
+
+    # Here we insert a column of 1s into training_data and test_data (to be consistent with our lecture slides)
+    train_data_intercept = np.insert(train_data, 0, 1, axis=1)
+    test_data_intercept = np.insert(test_data, 0, 1, axis=1)
+
+    # Here we call myGradientDescentFun to train the model and get weights
+    # Note: you need to figure out good learning rate value and the number of iterations
+    w = myGradientDescentFun(train_data_intercept, train_target, 0.01, 100000)
+
+    # Task 3-3: show intercept and coefficents
+    intercept = w[0]
+    weights = w[1:]
+    print('Intercept: ', intercept, '\nWeights: ', weights, '\n')
+
+    # Task 3-4: show errors on training dataset and testing dataset
+    train_predict = myPredictFun(train_data_intercept, w)
+    test_predict = myPredictFun(test_data_intercept, w)
+    print('Mean Squared Training Error: ', calcError(train_target, train_predict))
+    print('Mean Squared Testing Error: ', calcError(test_target, test_predict))
+
+    # Task 3-5: show plot a figure to show your predictions and real prices on the testing dataset
+    plot(test_target, test_predict)
